12639d7127158a48ed4253bde129988c3eb3e132
SamanAbbasi <samanabbasi.plus@gmail.com>
2024-06-02 11:07:22 +0330

    complete todoapp

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose')
 
 const app = express()
 app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    res.setHeader('Access-Control-Allow-Methods', '*')
+    next()
+})
 
 app.use('/api/todo', todoRoutes)
 
